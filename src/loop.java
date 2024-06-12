75626a54f4f05219ddca2ff04be47cd42e116a42
Amirov0011 <amirovdeyanet0011@gmail.com>
2024-06-12 15:46:30 +0400

    loop- Deyanet

---
+++ src/loop.java
@@ -0,0 +1,43 @@
+public class loop {
+        public static void main(String[] args) {
+
+                int result;
+
+                for (int i = 0; i <=10; i++){
+                        result=5*i;
+                                System.out.println("5*"+i+"="+ result);
+
+
+
+                        }
+
+
+
+
+
+
+                        }
+
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
